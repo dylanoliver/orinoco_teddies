716e2888113ff412f162e5a2646616e82c9244be
Dylan Oliver <dylanoliver@me.com>
2020-05-20 20:51:48 +0100

    Updated confirmation script/markup

--- confirmation.js
+++ confirmation.js
@@ -2,8 +2,16 @@ let params = new URLSearchParams(window.location.search);
 let id = params.get("id");
 let price = params.get("price");
 
+
+
 // Get DOM elements for showing response
 let totalPrice = document.getElementById('total-price');
 totalPrice.textContent = 'Total price:' + ' ' + price + '€';
 let responseOrderId = document.getElementById('response-id');
 responseOrderId.textContent = 'Order id:' + ' ' + id;
+
+request.onerror = function () {
+  // alert('Server error, please try again later');
+  const errorServer = document.getElementById('errorServer');
+  errorServer.textContent = 'There was an error connecting to the server. ';
+};
